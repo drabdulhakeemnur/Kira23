c3165d3bb20a06bb9f0534377396df1c7c1303f7
drabdulhakeemnur <drabdulhakeemnur@gmail.com>
2023-12-20 09:57:22 +0300

    Complete SIgnup page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,6 +22,9 @@ export default function Header() {
                 <Link to ='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                     </Link>
+                    <Link to='/sign-up'>
+                        <li className=' text-slate-700 hover:underline'>Sign Up</li>
+                    </Link>
                     <Link to='/sign-in'>
                         <li className=' text-slate-700 hover:underline'>Sign In</li>
                     </Link>
