8d750f983f9f1ca51aa040140309781ceb6c7dfb
drabdulhakeemnur <drabdulhakeemnur@gmail.com>
2023-12-20 12:24:48 +0300

    complee signup page functionality

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 // routes
 app.use('/api/user', userRouter)
-app.use('/api/user', authRouter)
+app.use('/api/auth', authRouter)
 app.use((err,req,res,next)=>{
     const statusCode= err.statusCode ||500
     const message= err.message || 'internal Server Error'
